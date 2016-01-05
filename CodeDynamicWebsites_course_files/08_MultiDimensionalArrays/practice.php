763062212472609889087ad904d407f610b3397e
Kitwana Akil <kakil@itelinc.com>
2016-01-05 16:22:06 -0500

    Updated with the completed practice for Lesson 8 MultiDimensional Arrays.

--- CodeDynamicWebsites_course_files/08_MultiDimensionalArrays/practice.php
+++ CodeDynamicWebsites_course_files/08_MultiDimensionalArrays/practice.php
@@ -1,20 +1,43 @@
 <?php
 	
 	// Constants
-	
+	define("Title", "MultiDimensional Arrays");
 	
 	// Custom Variables
+	$lessonNumber = 8;
+	$myName = "Kitwana";
+	
+	date_default_timezone_set('UTC');
 
 	
 	// Moustache Multi-Dimensional Array
-
+	$moustache = array(
+		
+		array(
+			name => "Handlebar",
+			creepFactor => "High",
+			avgGrowthInDays => 14
+		),
+		
+		array(
+			name => "Salvador Dali",
+			creepFactor => "Extreme",
+			avgGrowthInDays => 60
+		),
+		
+		array(
+			name => "Goatee",
+			creepFactor => "Low",
+			avgGrowthInDays => 3
+		)
+	);
 
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>PHP <!-- PAGE TITLE --></title>
+		<title>PHP <?php echo(Title); ?></title>
 		<link href="/assets/styles.css" rel="stylesheet">
 	</head>
 	<body>
@@ -23,14 +46,20 @@
 				<img src="/assets/img/logo.png" alt="PHP">
 			</a>
 			
-			<h1>Tutorial <!-- LESSON NUMBER -->: <small><!-- PAGE TITLE --></small></h1>
+			<h1>Tutorial <?php echo($lessonNumber); ?>: <small><?php echo(Title); ?></small></h1>
 			<hr>
 			
 			<h2>Your Example</h2>
 			
 			<div class="sandbox">
-				<h2>The <!-- MOUSTACHE NAME --> Moustache!</h2>
-				<p>This moustache is quite the dirt squirrel! It boasts a creep factor of <strong><!-- CREEP FACTOR --></strong> and takes <strong><!-- GROWTH DAYS --> days</strong> to grow on average.</strong></p>
+				<h2>The <?php echo $moustache[0][name]; ?> Moustache!</h2>
+				<p>This moustache is quite the dirt squirrel! It boasts a creep factor of <strong><?php echo $moustache[0][creepFactor]; ?></strong> and takes <strong><?php echo($moustache[0][avgGrowthInDays]); ?> days</strong> to grow on average.</strong></p>
+				
+				<h2>The <?php echo $moustache[1][name]; ?> Moustache!</h2>
+				<p>This moustache is quite the dirt squirrel! It boasts a creep factor of <strong><?php echo $moustache[1][creepFactor]; ?></strong> and takes <strong><?php echo($moustache[1][avgGrowthInDays]); ?> days</strong> to grow on average.</strong></p>
+				
+				<h2>The <?php echo $moustache[2][name]; ?> Moustache!</h2>
+				<p>This moustache is quite the dirt squirrel! It boasts a creep factor of <strong><?php echo $moustache[2][creepFactor]; ?></strong> and takes <strong><?php echo($moustache[2][avgGrowthInDays]); ?> days</strong> to grow on average.</strong></p>
 				
 				<!-- REPEAT ABOVE 2X -->
 				
